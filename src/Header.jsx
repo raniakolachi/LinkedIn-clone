838f58f662346b33bd4b4efe27a8277f43dc680f
raniakolachi <raniakolachi.4xwave@gmail.com>
2024-11-25 08:53:46 +0500

    I added API products.

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,64 +1,82 @@
-import React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import PersonIcon from '@mui/icons-material/Person';
-import LocalMallIcon from '@mui/icons-material/LocalMall';
-import { Link } from 'react-router-dom';
-import hero from './imges/slide-1 (1).jpg';
+import React, { useEffect, useState } from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import Badge from "@mui/material/Badge";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import PersonIcon from "@mui/icons-material/Person";
+import LocalMallIcon from "@mui/icons-material/LocalMall";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
+  "&:hover": {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginRight: theme.spacing(2),
   marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(3),
-    width: 'auto',
+    width: "auto",
   },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+const SearchIconWrapper = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
+  color: "inherit",
+  "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
     },
   },
 }));
 
 export default function Header() {
+  const [products, setProducts] = useState([]); // Data store karne ke liye state
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.escuelajs.co/api/v1/products"
+        );
+        setProducts(response.data); // Data ko state mein set kar rahe hain
+        console.log(response.data, "products");
+      } catch (error) {
+        console.error("Error fetching the product:", error);
+      }
+    };
+
+    fetchProduct();
+  }, []);
+
+  // Baaki ka Header code waise hi chalega
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
@@ -82,41 +100,46 @@ export default function Header() {
     setMobileMoreAnchorEl(event.currentTarget);
   };
 
-  const menuId = 'primary-search-account-menu';
+  const menuId = "primary-search-account-menu";
   const renderMenu = (
     <Menu
       anchorEl={anchorEl}
       anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       id={menuId}
       keepMounted
       transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       open={isMenuOpen}
       onClose={handleMenuClose}
     >
-      <MenuItem className='text-black' onClick={handleMenuClose} ><Link to={'/sign-in'}>Profile</Link></MenuItem>
-      <MenuItem onClick={handleMenuClose}> <Link to={'/my-account'}>My account</Link></MenuItem>
+      <MenuItem className="text-black" onClick={handleMenuClose}>
+        <Link to={"/sign-in"}>Profile</Link>
+      </MenuItem>
+      <MenuItem onClick={handleMenuClose}>
+        {" "}
+        <Link to={"/my-account"}>My account</Link>
+      </MenuItem>
     </Menu>
   );
 
-  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const mobileMenuId = "primary-search-account-menu-mobile";
   const renderMobileMenu = (
     <Menu
       anchorEl={mobileMoreAnchorEl}
       anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       id={mobileMenuId}
       keepMounted
       transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
@@ -124,7 +147,7 @@ export default function Header() {
       <MenuItem>
         <IconButton size="large" aria-label="show 4 new mails" color="inherit">
           <Badge badgeContent={4} color="error">
-            <FavoriteIcon  />
+            <FavoriteIcon />
           </Badge>
         </IconButton>
         <p>Messages</p>
@@ -136,7 +159,7 @@ export default function Header() {
           color="inherit"
         >
           <Badge badgeContent={17} color="error">
-            <PersonIcon/>
+            <PersonIcon />
           </Badge>
         </IconButton>
         <p>Notifications</p>
@@ -158,85 +181,101 @@ export default function Header() {
 
   return (
     <>
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ display: { xs: 'none', sm: 'block' } }}
-          >
-            Home
-          </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
-          <Box sx={{ flexGrow: 1 }} />
-          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-              <Badge badgeContent={4} color="error">
-                <FavoriteIcon  />
-              </Badge>
-            </IconButton>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
             <IconButton
               size="large"
-              aria-label="show 17 new notifications"
+              edge="start"
               color="inherit"
+              aria-label="open drawer"
+              sx={{ mr: 2 }}
             >
-              <Badge badgeContent={17} color="error">
-                <PersonIcon />
-              </Badge>
+              <MenuIcon />
             </IconButton>
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="inherit"
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ display: { xs: "none", sm: "block" } }}
             >
-              <AccountCircle />
-            </IconButton>
-          </Box>
-          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <LocalMallIcon/>
-            </IconButton>
-          </Box>
-        </Toolbar>
-      </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
-    </Box>
+              Home
+            </Typography>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ "aria-label": "search" }}
+              />
+            </Search>
+            <Box sx={{ flexGrow: 1 }} />
+            <Box sx={{ display: { xs: "none", md: "flex" } }}>
+              <IconButton
+                size="large"
+                aria-label="show 4 new mails"
+                color="inherit"
+              >
+                <Badge badgeContent={4} color="error">
+                  <FavoriteIcon />
+                </Badge>
+              </IconButton>
+              <IconButton
+                size="large"
+                aria-label="show 17 new notifications"
+                color="inherit"
+              >
+                <Badge badgeContent={17} color="error">
+                  <PersonIcon />
+                </Badge>
+              </IconButton>
+              <IconButton
+                size="large"
+                edge="end"
+                aria-label="account of current user"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                onClick={handleProfileMenuOpen}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+            </Box>
+            <Box sx={{ display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="show more"
+                aria-controls={mobileMenuId}
+                aria-haspopup="true"
+                onClick={handleMobileMenuOpen}
+                color="inherit"
+              >
+                <LocalMallIcon />
+              </IconButton>
+            </Box>
+          </Toolbar>
+        </AppBar>
+        {renderMobileMenu}
+        {renderMenu}
+      </Box>
+      {/* Displaying Product Data */}
+      <div className="container">
+  <h2>Products:</h2>
+  <div className="row">
+    {products.map((product) => (
+      <div className="col-md-3 col-sm-6" key={product.id}>
+        <div className="card">
+          <img src={product.images} className="card-img-top" alt={product.title} />
+          <div className="card-body">
+            <h5 className="card-title">{product.title}</h5>
+          </div>
+        </div>
+      </div>
+    ))}
+  </div>
+</div>
 
-    <div>
-    <img height={430} w-100 src={hero} alt="Hero" />
-    </div>
     </>
   );
 }
-
--- src/MyAccount.jsx
+++ src/MyAccount.jsx
@@ -1,63 +1,135 @@
-import { FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField, Button, Box } from "@mui/material";
-import { useState } from "react";
+import React, { useState } from "react";
+import {
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+  Button,
+  Box,
+} from "@mui/material";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
-const Signin = () => {
-    const [showPassword, setShowPassword] = useState(false);
+// Validation Schema
+const signinSchema = yup.object().shape({
+  firstName: yup.string().required("First Name is required"),
+  secondName: yup.string().required("Second Name is required"),
+  email: yup
+    .string()
+    .email("Invalid email format")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .matches(/^\d+$/, "Password should contain only numbers")
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+});
 
-    const handleClickShowPassword = () => {
-        setShowPassword(!showPassword);
-    };
+const Signup = () => {
+  const [showPassword, setShowPassword] = useState(false);
 
-    const handleMouseDownPassword = (event) => {
-        event.preventDefault();
-    };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(signinSchema),
+  });
 
-    return (
-        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", mt: 8 }}>
-            <h1>Sign In</h1>
-            <TextField
-                id="first-name"
-                label="First Name"
-                variant="filled"
-                sx={{ mt: 2, width: "25ch" }}
-            />
-            <TextField
-                id="second-name"
-                label="Second Name"
-                variant="filled"
-                sx={{ mt: 2, width: "25ch" }}
-            />
-            <FormControl sx={{ mt: 3, width: "25ch" }} variant="outlined">
-                <InputLabel htmlFor="password">Password</InputLabel>
-                <OutlinedInput
-                    id="password"
-                    type={showPassword ? "text" : "password"}
-                    endAdornment={
-                        <InputAdornment position="end">
-                            <IconButton
-                                aria-label="toggle password visibility"
-                                onClick={handleClickShowPassword}
-                                onMouseDown={handleMouseDownPassword}
-                                edge="end"
-                            >
-                                {showPassword ? <VisibilityOff /> : <Visibility />}
-                            </IconButton>
-                        </InputAdornment>
-                    }
-                    label="Password"
-                />
-            </FormControl>
-            <Button
-                variant="contained"
-                sx={{ mt: 3, width: "25ch" }}
-                onClick={() => alert("Sign In Button Clicked")}
-            >
-                Sign In
-            </Button>
-        </Box>
-    );
+  const onSubmit = (data) => {
+    console.log("Form Data:", data); // Logs all form data to console
+  };
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        mt: 8,
+      }}
+    >
+      <h1>Sign up</h1>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <TextField
+          id="first-name"
+          label="First Name"
+          variant="filled"
+          sx={{ mt: 2, width: "25ch" }}
+          {...register("firstName")}
+          error={!!errors.firstName}
+          helperText={errors.firstName?.message}
+        />
+        <br />
+        <TextField
+          id="second-name"
+          label="Second Name"
+          variant="filled"
+          sx={{ mt: 2, width: "25ch" }}
+          {...register("secondName")}
+          error={!!errors.secondName}
+          helperText={errors.secondName?.message}
+        />
+        <br />
+        <TextField
+          id="email"
+          label="Email"
+          variant="filled"
+          sx={{ mt: 2, width: "25ch" }}
+          {...register("email")}
+          error={!!errors.email}
+          helperText={errors.email?.message}
+        />
+        <br />
+        <FormControl sx={{ mt: 3, width: "25ch" }} variant="outlined">
+          <InputLabel htmlFor="password" error={!!errors.password}>
+            Password
+          </InputLabel>
+          <OutlinedInput
+            id="password"
+            type={showPassword ? "text" : "password"}
+            {...register("password")}
+            error={!!errors.password}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+          {errors.password && (
+            <p style={{ color: "red", fontSize: "12px" }}>
+              {errors.password.message}
+            </p>
+          )}
+        </FormControl>
+        <br />
+        <Button type="submit" variant="contained" sx={{ mt: 1, width: "25ch" }}>
+          Sign up
+        </Button>
+      </form>
+    </Box>
+  );
 };
 
-export default Signin;
+export default Signup;
